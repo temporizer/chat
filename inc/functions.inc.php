5e59a5b47c0e58dd3456894d1057fc27d0f4ee3d
temporizer <temporizer@gmail.com>
2014-07-07 16:16:30 -0700

    added getAllMessages() function

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -1,7 +1,22 @@
 <?php 
 	require_once("connect.inc.php");
+	session_start();
 
 
-	
+	function getAllMessages() {
+		global $dbc;
+		$messages = null;
+		$sql = "SELECT * FROM messages";
+		$query = mysqli_query($dbc, $sql);
+		$num_rows = mysqli_num_rows($query);
+		
+		if ($num_rows >= 1) {
+			for ($i=0; $i<$num_rows; $i++) {
+				$messages[] = mysqli_fetch_assoc($query);
+			}
+		}
+		
+		return $messages;
+	}
 
  ?>
--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include("inc/header.inc.php"); ?>
 
+<?php 
+	$messages = getAllMessages();
+	echo "<pre>";
+	print_r($messages);
+	echo "</pre>";
 
+ ?>
 
 <?php include("inc/footer.inc.php"); ?>
